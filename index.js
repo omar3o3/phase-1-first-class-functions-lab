60789b8b922c5f4c325808f5f16a212144697f02
Omar <otantawy53@gmail.com>
2022-04-26 12:49:28 -0400

    commit tree 8c8e6235cca972f2ffe9d9fb7b813ec408cb5fe1

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Code your solution in this file!
+// const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (y){
+  return y.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (x){
+  return x.slice(-2);
+}
+
+// let selectingDrivers = [
+//   returnFirstTwoDrivers(x)
+// ]
+
+let selectingDrivers = [
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers
+]
+
+const createFareMultiplier = function fareMultiplier (multiplier){
+  return function(fare){
+    return multiplier * fare;
+  }
+}
+
+// function fareDoubler(x){
+//   return x * 2;
+// }
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+// const selectDifferentDrivers = function (arrayOfDrivers, whichDrivers){
+//   if (whichDrivers === returnFirstTwoDrivers){
+//     return returnFirstTwoDrivers;
+//   } else if (whichDrivers === returnLastTwoDrivers){
+//     return returnLastTwoDrivers;
+//   }
+// }
+
+// const selectDifferentDrivers = function(arrayOfDrivers, whichDrivers){
+//   return whichDrivers(arrayOfDrivers);
+// }
+
+const selectDifferentDrivers = function(arrayOfDrivers, whichDrivers){
+  if (whichDrivers === returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(arrayOfDrivers);
+  } else if (whichDrivers === returnLastTwoDrivers){
+    return returnLastTwoDrivers(arrayOfDrivers);
+  }
+}
